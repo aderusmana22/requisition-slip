4557fd17b08226bb291bcfc20e331a940c4f3b61
Zidannn10 <zidanazzahra916@gmail.com>
2025-11-17 15:54:55 +0700

    tracking path pindah posisi dibawah approval path

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -37,8 +37,8 @@
                 </a>
                 <ul class="collapse" id="master-management">
                     <li><a href="{{ route('requisition.path') }}">Approval Path</a></li>
-                    <li><a href="{{ route('master.revision.index') }}">Revision</a></li>
                     <li><a href="{{ route('master.tracking-path.index') }}">Tracking Path</a></li>
+                    <li><a href="{{ route('master.revision.index') }}">Revision</a></li>
 
                 </ul>
             </li>
